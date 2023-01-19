023fccb704af745c871c54a0d728b422d9b4d713
Isabelle Nguyen <isabelln@usc.edu>
2023-01-18 21:02:27 -0800

    another

--- epi_judge_cpp/is_string_palindromic_punctuation.cc
+++ epi_judge_cpp/is_string_palindromic_punctuation.cc
@@ -3,7 +3,21 @@
 #include "test_framework/generic_test.h"
 using std::string;
 bool IsPalindrome(const string& s) {
-  // TODO - you fill in here.
+  int i = 0, j = s.size() - 1;
+  while (i < j) {
+    char c1 = s[i], c2 = s[j];
+    int n1 = s[i] - '0', n2 = s[j] - '0';
+    while (i < j && !((c1 >= 'A' && c1 <= 'Z')  || (c1 >= 'a' && c1 <= 'z') || (n1 >= 0 && n1 <= 9))) {
+      c1 = s[++i];
+      n1 = s[i] - '0';
+    }
+    while (i < j && !((c2 >= 'A' && c2 <= 'Z') || (c2 >= 'a' && c2 <= 'z') || (n2 >= 0 && n2 <= 9))) {
+      c2 = s[--j];
+      n2 = s[j] - '0';
+    }
+    if (std::tolower(c1) != std::tolower(c2) && n1 != n2) return false;
+    i++, j--;
+  }
   return true;
 }
 
--- epi_judge_cpp/reverse_words.cc
+++ epi_judge_cpp/reverse_words.cc
@@ -1,12 +1,43 @@
 #include <string>
+#include <algorithm>
+#include <iostream>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
 using std::string;
 
 void ReverseWords(string* s) {
-  // TODO - you fill in here.
-  return;
+  string& a = *s;
+
+  std::reverse(a.begin(), a.end());
+
+  int i = 0;
+
+  while (i < a.size()) {
+    int j = i;
+    while (j < a.size() && a[j] != ' ') j++;
+    std::reverse(a.begin() + i, a.begin() + j);
+    if (a[j] == ' ') j++;
+    i = j;
+  }
+
+//  string w, ans;
+//
+//  for (int i = a.size() - 1; i >= 0; --i) {
+//    if (a[i] != ' ') w += a[i];
+//    else {
+//      std::reverse(w.begin(), w.end());
+//      ans.append(w + " ");
+//      w = "";
+//    }
+//  }
+//
+//  std::reverse(w.begin(), w.end());
+//  ans.append(w);
+//
+//  for (int i = 0; i < a.size(); ++i) {
+//    a[i] = ans[i];
+//  }
 }
 string ReverseWordsWrapper(TimedExecutor& executor, string s) {
   string s_copy = s;
