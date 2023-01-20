53b1f3ea6348e3029850afab027cd034cf7c045e
Isabelle Nguyen <isabelln@usc.edu>
2023-01-19 18:12:58 -0800

    look

--- epi_judge_cpp/look_and_say.cc
+++ epi_judge_cpp/look_and_say.cc
@@ -1,11 +1,39 @@
 #include <string>
-
+#include <cmath>
+#include <iostream>
 #include "test_framework/generic_test.h"
 using std::string;
 
 string LookAndSay(int n) {
-  // TODO - you fill in here.
-  return "";
+  string ans = "", prev = "1";
+  int count = 1;
+
+  if (n == 1) return "1";
+
+  while (count < n) {
+    ans = "";
+    char start = '-';
+    int repeat = 0, index = prev.size() - 1;
+    std::reverse(prev.begin(), prev.end());
+    while (prev.size()) {
+      char comp = prev[index--];
+      if (comp != start) {
+        if (start != '-') {
+          ans.push_back(repeat + '0');
+          ans.push_back(start);
+        }
+        start = comp;
+        repeat = 1;
+      } else repeat++;
+      prev.pop_back();
+    }
+    ans.push_back(repeat + '0');
+    ans.push_back(start);
+    count++;
+    prev = ans;
+  }
+
+  return ans;
 }
 
 int main(int argc, char* argv[]) {
