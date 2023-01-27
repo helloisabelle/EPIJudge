c3deaa3ec4361ab34ac5e46891c81ce2b68150ed
Isabelle Nguyen <isabelln@usc.edu>
2023-01-26 20:47:48 -0800

    eval

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -1,10 +1,34 @@
 #include <string>
+#include <stack>
+#include <ctype.h>
+#include <iostream>
 
 #include "test_framework/generic_test.h"
-using std::string;
-int Evaluate(const string& expression) {
-  // TODO - you fill in here.
-  return 0;
+
+int Evaluate(const std::string& expression) {
+  std::stack<int> s;
+  std::string copy = expression;
+  std::string delimiter = ",";
+
+  while (copy.size()) {
+    std::string token = copy;
+    if (copy.find(delimiter) != -1) token = copy.substr(0, copy.find(delimiter));
+    if (token.size() == 1 && !isdigit(token[0])) {
+      int b = s.top();
+      s.pop();
+      int a = s.top();
+      s.pop();
+      if (token == "/") s.emplace(a / b);
+      else if (token == "*") s.emplace(a * b);
+      else if (token == "-") s.emplace(a - b);
+      else if (token == "+") s.emplace(a + b);
+    } else s.emplace(std::stoi(token));
+    if (copy.find(delimiter) != -1) {
+      copy = copy.substr(copy.find(delimiter) + 1, copy.size());
+    } else break;
+  }
+eval
+  return s.top();
 }
 
 int main(int argc, char* argv[]) {
