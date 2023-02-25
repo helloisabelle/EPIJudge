10e49c58dcbd98e5232063a817fdad82eb6a8bf1
Isabelle Nguyen <isabelln@usc.edu>
2023-02-25 15:28:09 -0800

    small subarr

--- epi_judge_cpp/smallest_subarray_covering_set.cc
+++ epi_judge_cpp/smallest_subarray_covering_set.cc
@@ -1,6 +1,9 @@
 #include <string>
 #include <unordered_set>
 #include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <list>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
@@ -15,8 +18,52 @@ struct Subarray {
 
 Subarray FindSmallestSubarrayCoveringSet(
     const vector<string> &paragraph, const unordered_set<string> &keywords) {
-  // TODO - you fill in here.
-  return {0, 0};
+    std::list<int> loc;
+    std::unordered_map<string, std::list<int>::iterator> map;
+    Subarray res = {-1, -1};
+
+    for (auto w : keywords) map[w] = loc.end();
+
+    auto it = paragraph.begin();
+    int i = 0;
+
+    while (i != size(paragraph)) {
+      string s = *it++;
+      if (map.find(s) != map.end()) {
+        if (map[s] != loc.end()) loc.erase(map[s]);
+
+        loc.emplace_back(i);
+        map[s] = --loc.end();
+
+        if (size(loc) == size(keywords)) {
+          if (res.start + res.end < 0 || res.end - res.start > i - loc.front())
+            res = {loc.front(), i};
+        }
+      }
+      ++i;
+    }
+
+//  std::unordered_map<string, int> map;
+//  int still_looking = keywords.size();
+//  Subarray res = {-1, -1};
+//
+//  for (auto w : keywords) ++map[w];
+//
+//  for (int l = 0, r = 0; r < paragraph.size(); ++r) {
+//    if (keywords.find(paragraph[r]) != keywords.end() && map[paragraph[r]]-- > 0) {
+//      --still_looking;
+//    }
+//
+//    while (still_looking == 0) {
+//      if (res.start + res.end < 0 || res.end - res.start > r - l) res = {l, r};
+//      if (keywords.count(paragraph[l]) && ++map[paragraph[l]] > 0) {
+//        ++still_looking;
+//      }
+//      ++l;
+//    }
+//  }
+//
+  return res;
 }
 int FindSmallestSubarrayCoveringSetWrapper(
     TimedExecutor &executor, const vector<string> &paragraph,
