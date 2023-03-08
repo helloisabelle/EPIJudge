80a3809a7fdc7b930f3dea47141780628da3d4d8
Isabelle Nguyen <isabelln@usc.edu>
2023-03-08 15:45:16 -0800

    nqueens

--- epi_judge_cpp/n_queens.cc
+++ epi_judge_cpp/n_queens.cc
@@ -1,13 +1,43 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include <cmath>
 
 #include "test_framework/generic_test.h"
 using std::vector;
 
+bool IsValid(const vector<int>& col) {
+  int new_queen_idx = size(col) - 1;
+  for (int i = 0; i < new_queen_idx; ++i) {
+    // difference between queen here and most recently placed queen
+    int diff = std::abs(col[i] - col[new_queen_idx]);
+    // on same column or on diagonal
+    if (diff == 0 || diff == new_queen_idx - i) return false;
+  }
+  return true;
+}
+
+void helper(int n, int row, vector<int>& col, vector<vector<int>>& res) {
+  if (n == row) {
+    // we have placed a queen on every row
+    res.emplace_back(col);
+  } else {
+    for (int i = 0; i < n; i++) {
+      col.emplace_back(i);
+      // trying placing a queen here
+      if (IsValid(col)) {
+        helper(n, row + 1, col, res);
+      }
+      col.pop_back();
+    }
+  }
+}
+
 vector<vector<int>> NQueens(int n) {
-  // TODO - you fill in here.
-  return {};
+  vector<vector<int>> ans;
+  std::vector<int> col;
+  helper(n, 0, col, ans);
+  return ans;
 }
 
 int main(int argc, char* argv[]) {
